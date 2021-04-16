b4afb6603f8a1a69f1b40a4341ea2b3178f34de4
ThienThanh2911 <1851050130thanh@ou.edu.vn>
2021-04-16 20:25:28 +0700

    Update new form

--- BobaApp/src/main/java/com/boba/bobaapp/MainController.java
+++ BobaApp/src/main/java/com/boba/bobaapp/MainController.java
@@ -32,8 +32,6 @@ import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
