a49ea91e65698e8a3880de5dc426c71f091e106e
ThienTuuIT82 <1851050179tuu@ou.edu.vn>
2021-05-06 14:51:31 +0700

    Update ProductTest

--- BobaApp/src/test/java/com/boba/service/ProductTester.java
+++ BobaApp/src/test/java/com/boba/service/ProductTester.java
@@ -19,15 +19,16 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.TestMethodOrder;
 
 /**
  *
  * @author Admin
  */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ProductTester {
     private static Connection conn;
     
@@ -51,6 +52,7 @@ public class ProductTester {
     }
     
     @Test
+    @Order(1)
     @DisplayName("Kiểm thử tìm kiếm đúng tên sản phẩm")
     public void testWithKeyword() throws SQLException {
         ProductService s = new ProductService(conn);
@@ -61,6 +63,7 @@ public class ProductTester {
     }
     
     @Test
+    @Order(2)
     @DisplayName("Kiểm thử tìm kiếm sai tên sản phẩm")
     public void testWithUnknowKeyword() throws SQLException {
         ProductService s = new ProductService(conn);
@@ -70,6 +73,7 @@ public class ProductTester {
     }
     
     @Test
+    @Order(3)
     @DisplayName("Kiểm thử tìm kiếm đúng id sản phẩm")
     public void testSearchProductWithId() throws SQLException {
         ProductService s = new ProductService(conn);
@@ -82,6 +86,7 @@ public class ProductTester {
     }
     
     @Test
+    @Order(4)
     @DisplayName("Kiểm thử tìm kiếm sai id sản phẩm")
     public void testSearchProductWithInvalidId() throws SQLException {
         ProductService s = new ProductService(conn);
@@ -90,6 +95,7 @@ public class ProductTester {
     }
     
     @Test
+    @Order(5)
     @DisplayName("Kiểm thử ném đúng ngoại lệ mong muốn")
     public void testException() throws SQLException {
         Assertions.assertThrows(SQLDataException.class, () -> {
@@ -99,6 +105,7 @@ public class ProductTester {
     }
     
     @Test
+    @Order(6)
     @DisplayName("Kiểm thử thêm sản phẩm với tên không hợp lệ")
     public void testAddProductWithInvalidName() {
         ProductService s = new ProductService(conn);
@@ -111,6 +118,7 @@ public class ProductTester {
     }
     
     @Test
+    @Order(7)
     @DisplayName("Kiểm thử thêm sản phẩm")
     public void testAddProduct() {
         ProductService s = new ProductService(conn);
@@ -130,20 +138,23 @@ public class ProductTester {
     }
     
     @Test
+    @Order(8)
     @DisplayName("Kiểm thử sửa sản phẩm với id không hợp lệ")
-    public void testUpdateProductWithInvalidId() {
+    public void testUpdateProduct() {
         try {
             ProductService s = new ProductService(conn);
             
-            products = s.getProducts("");
+            List <Product> products = s.getProducts("");
+            products.get(0).setName("ưqas");
             
-            Assertions.assertFalse(s.updateProduct(products.get(999)));
+            Assertions.assertTrue(s.updateProduct(products.get(0)));
         } catch (SQLException ex) {
             Logger.getLogger(ProductTester.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
     @Test
+    @Order(9)
     @DisplayName("Kiểm thử xóa sản phẩm với id không hợp lệ")
     public void testRemoveProductWithInvalidId() {
         ProductService s = new ProductService(conn);
@@ -152,11 +163,20 @@ public class ProductTester {
     }
 
     @Test
+    @Order(10)
     @DisplayName("Kiểm thử xóa sản phẩm")
     public void testRemoveProduct() {
         ProductService s = new ProductService(conn);
         
-        Assertions.assertFalse(s.deleteProduct(5));
+        List <Product> products;
+        try {
+            products = s.getProducts("");
+        
+            
+        Assertions.assertTrue(s.deleteProduct(products.get(0).getId()));
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductTester.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
